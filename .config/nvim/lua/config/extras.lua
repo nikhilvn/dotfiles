08a4f06d2893807ec5ef77195309412280d6db36
nikhilvn <nvnofficial13@gmail.com>
2025-08-23 04:43:40 +0530

    fix(neovim): fixed vue-ts not working on v3.0

--- .config/nvim/lua/config/extras.lua
+++ .config/nvim/lua/config/extras.lua
@@ -1,10 +1,10 @@
 return {
   -- Languages
+  -- { import = "lazyvim.plugins.extras.lang.vue" },
   { import = "lazyvim.plugins.extras.lang.typescript" },
+  { import = "lazyvim.plugins.extras.lang.tailwind" },
   { import = "lazyvim.plugins.extras.lang.json" },
   { import = "lazyvim.plugins.extras.lang.markdown" },
-  { import = "lazyvim.plugins.extras.lang.tailwind" },
-  { import = "lazyvim.plugins.extras.lang.vue" },
 
   -- Misc
   { import = "lazyvim.plugins.extras.linting.eslint" },
--- .config/nvim/lua/plugins/blink-cmp.lua
+++ .config/nvim/lua/plugins/blink-cmp.lua
@@ -1,24 +1,6 @@
 return {
   "saghen/blink.cmp",
-  version = not vim.g.lazyvim_blink_main and "*",
-  build = vim.g.lazyvim_blink_main and "cargo build --release",
-  opts_extend = {
-    "sources.completion.enabled_providers",
-    "sources.compat",
-    "sources.default",
-  },
-  dependencies = {
-    "rafamadriz/friendly-snippets",
-    -- add blink.compat to dependencies
-    {
-      "saghen/blink.compat",
-      optional = true, -- make optional so it's only enabled if any extras need it
-      opts = {},
-      version = not vim.g.lazyvim_blink_main and "*",
-    },
-  },
   event = "InsertEnter",
-
   ---@param opts blink.cmp.Config | { sources: { compat: string[] } }
   opts = function(_, opts)
     opts.completion.documentation.auto_show = false
