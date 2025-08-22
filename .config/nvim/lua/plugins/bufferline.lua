f1e57602790ffa4fdd6a416a8028d0b74f6d0d31
nikhilvn <nvnofficial13@gmail.com>
2025-08-23 04:44:36 +0530

    refactor(neovim): changed bufferline configs

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -3,11 +3,9 @@ return {
   event = "VeryLazy",
   opts = function(_, opts)
     opts.options.always_show_bufferline = true
-    opts.options.indicator = {
-      -- icon = "▎",
-      icon = "", -- this should be omitted if indicator style is not 'icon'
-      style = "none", -- Options: 'icon', 'underline', 'none'
-    }
+    opts.options.minimum_padding = 1
+    opts.options.maximum_padding = 5
+    opts.options.maximum_length = 15
     opts.options.separator_style = { "", "" } -- | "thick" | "thin" | { 'any', 'any' },
     opts.options.truncate_names = true
     opts.options.offsets = {
@@ -23,13 +21,6 @@ return {
         highlight = "Directory",
         separator = true, -- use a "true" to enable the default, or set your own character
       },
-      -- {
-      --   filetype = "snacks_layout_box",
-      --   separator = true, -- use a "true" to enable the default, or set your own character
-      -- },
     }
-    opts.options.minimum_padding = 1
-    opts.options.maximum_padding = 5
-    opts.options.maximum_length = 15
   end,
 }
